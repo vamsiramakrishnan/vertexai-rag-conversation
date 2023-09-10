86e4eb21ce7ffae4433112c7cee1a268838a061d
Shanky Ram <shankyram@google.com>
2023-09-10 03:30:59 +0000

    updated wip

--- projects/streamlit-demos/helloworld/helloworld_main.py
+++ projects/streamlit-demos/helloworld/helloworld_main.py
@@ -1,1 +1,6 @@
-print("Hi")
+import streamlit as st
+from audio_recorder_streamlit import audio_recorder
+
+audio_bytes = audio_recorder()
+if audio_bytes:
+    st.audio(audio_bytes, format="audio/wav")
